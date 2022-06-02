b31942bd35273c08e39cc85b8f3ee5e58dc1ce82
Amirhossein Moghaddam <103887474+amirhosseinmoghaddam@users.noreply.github.com>
2022-06-02 12:20:25 +0430

    Update request_weather.py

--- Weatherapp/weatherapp1/request_weather.py
+++ Weatherapp/weatherapp1/request_weather.py
@@ -2,14 +2,19 @@ from unicodedata import name
 import requests
 
 def WeatherStatus(cities):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={YOUR_API_KEY}'
+    #Use API
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={ENTER_YOUR_API_KEY}'
     all_data = []
-
+    
+    #Get cities live weather
     for city in cities:
         r = requests.get(url.format(city)).json()
+        #Get temperature
         temp = r['main']['temp']
+        #Round to a Integer
         temp = round(temp)
-
+        
+        #Save all city weather datas in dict
         city_weather = {
             'name': city,
             'temp': temp,
